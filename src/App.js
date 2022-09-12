c6fa83164da5d3e05cab2683b8156bdd7eaee67d
AnnaKoziol1997 <85227390+AnnaKoziol1997@users.noreply.github.com>
2022-09-12 10:19:34 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 //import logo from './logo.svg';
 import './App.css';
-import { Button, Box} from '@mui/material';
+import { Button, Box, TextField} from '@mui/material';
 import { Stack} from '@mui/system';
 
 function App() {
   return (
     <Box textAlign='center'>
     <Stack display='block' spacing={2} direction='row'>
-      
+    <TextField id="standard-basic" label="Standard" variant="standard">
+
+    </TextField>
         <Button variant='contained' color='primary'size='small'>
-        Text
+        Button
         </Button>
         <Button variant='contained' color='secondary' size='small'>
           Text2
